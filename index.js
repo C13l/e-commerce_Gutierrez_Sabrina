b8e5dfe00625727cd9142563edadfff19c80081f
Ciel <Ciel@gmail.com>
2024-08-29 20:49:36 -0300

    Card JS

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Bienvenidos");
+const h5 = document.querySelector("h5")
+const text = document.querySelector(".card-text")
+const img = document.querySelector(".card-img-top");
+h5.innerText = "Paisaje";
+text.innerText = "El más bonito del mundo"
+img.src = "https://i.pinimg.com/736x/ac/53/c7/ac53c746c0b570df28f47c53f84657e6.jpg";
