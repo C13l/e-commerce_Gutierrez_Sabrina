28fb470f06c1db96fab7dda8937adac1829c481a
Ciel <Ciel@gmail.com>
2024-09-12 20:43:32 -0300

    Clase-10

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const h1 = document.querySelector("h1")
 h1.innerText = "Productos";
 
-const arraycard =[];
+function generarcards(){
 
-for(let i=1; i < 10; i++){
-        let card = `<div class="card card-margin" style="width: 18rem;">
-        <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top img-card" alt="${i}">
-        <div class="card-body">
-          <h5 class="card-title">Card title ${i}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Buy</a>
+    const arraycard =[];
+
+    for(let i=1; i < 10; i++){
+            let card = `<div class="card card-margin" style="width: 18rem;">
+            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top img-card" alt="${i}">
+            <div class="card-body">
+              <h5 class="card-title">Card title ${i}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Ver más</a>
+            </div>
         </div>
-    </div>
-    `;
-    arraycard.push(card)
+        `;
+        arraycard.push(card)
+    }
 
+    document.querySelector("section").innerHTML = arraycard.join('');
 }
 
-document.querySelector("section").innerHTML = arraycard.join('');
+generarcards();
